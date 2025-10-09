29274b2564843944275335b00ba7a60d8909378b
Rafael Ronsoni Gaidzinski <rafael.513011@alunosatc.edu.br>
2025-10-08 21:07:53 -0300

    Implementado Composite

---
+++ composite/practiceProblem.js
@@ -0,0 +1,65 @@
+class Components {
+  draw() {
+    throw new Error("Método abstrato");
+  }
+}
+
+// Componentes simples
+class Button extends Components {
+  constructor(label) {
+    super();
+    this.label = label;
+  }
+
+  draw() {
+    console.log(`Botão: [${this.label}]`);
+  }
+}
+
+class Text extends Components {
+  constructor(content) {
+    super();
+    this.content = content;
+  }
+
+  draw() {
+    console.log(`Texto: "${this.content}"`);
+  }
+}
+
+// Painel precisa saber o tipo de cada item manualmente
+class Panel {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  add(child) {
+    this.children.push(child);
+  }
+
+  render() {
+    console.log(`Painel: ${this.name}`);
+    this.children.forEach((child) => {
+      if (child instanceof Button) {
+        child.draw();
+      } else if (child instanceof Text) {
+        child.draw();
+      } else if (child instanceof Panel) {
+        child.render();
+      }
+    });
+  }
+}
+
+// Cliente
+const loginPanel = new Panel("Painel de Login");
+loginPanel.add(new Text("Tela de Login"));
+loginPanel.add(new Button("Entrar"));
+loginPanel.add(new Button("Cancelar"));
+
+const mainPanel = new Panel("Janela Principal");
+mainPanel.add(loginPanel);
+mainPanel.add(new Text("Versão 1.0.0"));
+
+mainPanel.render();
